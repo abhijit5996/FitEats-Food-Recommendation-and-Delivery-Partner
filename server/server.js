d0d099a0269b818f32a570addd07ef0bf7e4e68e
abhijit5996 <abhijitdasRahul010304@gmai.com>
2025-09-10 11:43:05 +0530

    client url add

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'https://zesty-life-two.vercel.app', 
+    'https://zesty-life-two.vercel.app',
+    'https://zesty-life-food-recommendation-and.vercel.app',
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173'
